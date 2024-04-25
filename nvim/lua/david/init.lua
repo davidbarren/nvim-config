edb771b00dff891f582414c2cf4fd680efca5b9d
David Barrenechea <davidbarrenechea98@gmail.com>
2024-04-25 13:17:31 +0300

    Added config files for nvim

---
+++ nvim/lua/david/init.lua
@@ -0,0 +1,4 @@
+require('david.set')
+require('david.remap')
+require('david.newcfile')
+require('david.newhfile')
---
+++ nvim/lua/david/set.lua
@@ -0,0 +1,38 @@
+vim.opt.guicursor = ''
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = '80'
+
+vim.opt.updatetime = 50
+
+vim.opt.showmode = false
+
+local header = require('42header')
+header.setup({
+	lengt = 80,
+	margin = 5,
+	default_map = true,
+	auto_update = true,
+	user = 'dbarrene',
+	mail = 'dbarrene@student.hive.fi',
+})
